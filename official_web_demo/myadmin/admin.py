35037c2a2da65c7a1ecc82a37b324607136274eb
CY <2856854019@qq.com>
2019-11-12 11:17:56 +0800

    企业官网示例 组合搜索 多对多优化

--- official_web_demo/myadmin/admin.py
+++ official_web_demo/myadmin/admin.py
@@ -4,9 +4,7 @@ from .models import Direction, Classification, Level, Video
 
 class VideoAdmin(admin.ModelAdmin):
     # 在admin后台中显示的字段，不可加上多对多关系
-    list_display = ('id', 'status', 'title', 'summary', 'img', 'href', 'level', 'classification', 'direction')
-    # 可编辑
-    list_editable = ['direction']
+    list_display = ('id', 'status', 'title', 'summary', 'img', 'href', 'level', 'classification')
 
 
 admin.site.register(Direction)
